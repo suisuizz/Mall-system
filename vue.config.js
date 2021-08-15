29b9ed99517d1f8ee62348122624229ef296da63
suisuiz <wangyumin1996@163.com>
2021-08-15 12:49:58 +0800

    feat：配置axios

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	productionSourceMap: false,
+	// devServer: {
+	// 	proxy: {
+	// 		'/api': {
+	// 			//代理api
+	// 			target: 'http://www.ysqorz.top:8888/api/private/v1/', // 代理接口(注意只要域名就够了)
+	// 			changeOrigin: true, //是否跨域
+	// 			ws: true, // proxy websockets
+	// 			pathRewrite: {
+	// 				//重写路径
+	// 				'^': '/api' //代理路径
+	// 			}
+	// 		}
+	// 	}
+	// }
+};
