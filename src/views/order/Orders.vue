04fcda5b1781a2c259f1054d61b479865fe1122f
suisuiz <wangyumin1996@163.com>
2021-09-13 23:13:28 +0800

    feat: 订单

--- src/views/order/Orders.vue
+++ src/views/order/Orders.vue
@@ -3,7 +3,7 @@
  * @Author: SUI
  * @Date: 2021-08-24 01:15:51
  * @LastEditors: SUI
- * @LastEditTime: 2021-08-24 01:18:52
+ * @LastEditTime: 2021-09-12 20:00:38
  * @FilePath: \mall-system-gitee\src\views\order\Orders.vue
 -->
 <template>
@@ -12,7 +12,58 @@
     <Bread :title="breadTitle"></Bread>
 
     <!-- 内容展示卡片 -->
-    <el-card class="box-card"> </el-card>
+    <el-card class="box-card">
+      <!-- 搜索框 -->
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+      <!-- 内容 -->
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="primary" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showDialog"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini" @click="showProgressBox"></el-button>
+          </template>
+        </el-table-column>
+
+        <!-- 分页选择器 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-sizes="[10, 15, 20, 30]"
+          :page-size="queryInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        >
+        </el-pagination>
+
+        <!-- 编辑 -->
+
+        <!-- 地址 -->
+      </el-table>
+    </el-card>
   </div>
 </template>
 <script>
@@ -29,13 +80,63 @@ export default {
       breadTitle: {
         one: '订单管理',
         two: '订单列表'
-      }
+      },
+
+      // 总条数
+      total: 0,
+
+      // 查询数据
+      queryInfo: {
+        query: '',
+        pagesize: 10,
+        pagenum: 1
+      },
+
+      // 数据展示
+      orderList: []
     }
   },
 
-  created() {},
+  created() {
+    // 获取订单列表
+    this.getOrderList()
+  },
 
-  methods: {}
+  methods: {
+    // 获取订单列表
+    getOrderList() {
+      let that = this
+      that.$api.get('orders', that.queryInfo, (res) => {
+        if (res.meta.status !== 200) return that.$message.error('获取数据失败')
+        that.$message.success('获取商订单数据成功')
+        // console.log(res.data)
+        // 商品总数
+        that.total = res.data.total
+        // 商品列表
+        that.orderList = res.data.goods
+      })
+    },
+
+    // 分页
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    },
+
+    // 编辑
+    showDialog() {
+      console.log('编辑')
+    },
+
+    // 地址
+    showProgressBox() {
+      console.log('地址')
+    }
+  }
 }
 </script>
 
